85974df51be04ab4cc14bddded80802044b2b2f0
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-04-25 15:18:52 -0400

    Weather API sample

---
+++ web/pages/api/weather.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default  (req, res) => {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=toronto&appid={API KEY HERE}")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          var temp = (result.main.temp - 273.15).toFixed(1);
+          res.status(200).json({temp: temp});
+        }
+      );
+}
