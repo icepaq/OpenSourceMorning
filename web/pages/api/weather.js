c426e94119e0ca504ddefb3e24ff21bd4ad792bd
Anton <62949848+TheIcepack@users.noreply.github.com>
2021-04-25 15:20:45 -0400

    Weather API sample

--- web/pages/api/weather.js
+++ web/pages/api/weather.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+
+// If someone could add asynchronous support that would be great
 export default  (req, res) => {
     fetch("http://api.openweathermap.org/data/2.5/weather?q=toronto&appid={API KEY HERE}")
       .then(response => response.json())
